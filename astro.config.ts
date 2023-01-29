b90a4795961b5a48766f470dab0790fdb377ebbf
edwardspresume <edwardspresume@gmail.com>
2023-01-29 17:38:57 -0500

    added partytown

--- astro.config.ts
+++ astro.config.ts
@@ -6,14 +6,13 @@ import sitemap from '@astrojs/sitemap';
 import astroI18next from 'astro-i18next';
 
 // https://astro.build/config
+import partytown from "@astrojs/partytown";
+
+// https://astro.build/config
 export default defineConfig({
-    site: 'https://hip.support/',
-    integrations: [
-        image({
-            cacheDir: './.cache/image',
-            serviceEntryPoint: '@astrojs/image/sharp',
-        }),
-        astroI18next(),
-        sitemap(),
-    ],
+  site: 'https://hip.support/',
+  integrations: [image({
+    cacheDir: './.cache/image',
+    serviceEntryPoint: '@astrojs/image/sharp'
+  }), astroI18next(), sitemap(), partytown()]
 });
