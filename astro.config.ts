ba453816dc94781301b267de49375f1707f42582
edwardspresume <edwardspresume@gmail.com>
2023-01-30 09:15:54 -0500

    updated config

--- astro.config.ts
+++ astro.config.ts
@@ -1,20 +1,33 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 import image from '@astrojs/image';
+import partytown from '@astrojs/partytown';
 import sitemap from '@astrojs/sitemap';
 import astroI18next from 'astro-i18next';
 
 // https://astro.build/config
-import partytown from "@astrojs/partytown";
-
-// https://astro.build/config
-
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://hip.support/',
-  integrations: [image({
-    cacheDir: './.cache/image',
-    serviceEntryPoint: '@astrojs/image/sharp'
-  }), astroI18next(), sitemap(), partytown()]
+    site: 'https://hip.support',
+    integrations: [
+        image({
+            cacheDir: './.cache/image',
+            serviceEntryPoint: '@astrojs/image/sharp',
+        }),
+        astroI18next(),
+        partytown({
+            config: {
+                debug: false,
+                forward: ['dataLayer.push'],
+            },
+        }),
+        sitemap({
+            i18n: {
+                defaultLocale: 'en',
+                locales: {
+                    en: 'en-US',
+                    ht: 'ht',
+                },
+            },
+        }),
+    ],
 });
